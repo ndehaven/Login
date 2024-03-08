68f71c492ddbd7de49372800315162665faa74bd
Ryan Fernandes <91709360+ryaniron@users.noreply.github.com>
2024-03-08 14:27:15 -0700

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,13 @@
+package dev.ryan.hinnalogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
